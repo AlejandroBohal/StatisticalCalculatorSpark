47dd97cce8cf4f2fff73c67e329a154aa097452f
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-08-20 02:44:24 -0500

    Correción calculos

--- src/main/resources/js/action.js
+++ src/main/resources/js/action.js
@@ -1,8 +1,7 @@
-console.log('hola');
-$(document).on("click","#buttonB",() =>{
+$("#buttonB").click(() =>{
     let numbers = document.getElementById('numbers').value.toString();
     const url = 'https://fathomless-bayou-96611.herokuapp.com/calculate/';
-    $.ajax({
+    jQuery.ajax({
         url: url,
         type:'POST',
         data: numbers,
@@ -11,10 +10,6 @@ $(document).on("click","#buttonB",() =>{
             let data = JSON.parse(response.data);
             $("#mean").append("Mean: " + data.mean);
             $("#std").append("Standard Deviation: " + data.std);
-        },
-        error: (error) =>{
-            console.log(error);
         }
     });
-
-})
+});
