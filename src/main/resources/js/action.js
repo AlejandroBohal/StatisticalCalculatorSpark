a0664c4e058f35d03031e25a1fcf49d77123209f
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-08-21 11:41:41 -0500

    Readme

--- src/main/resources/js/action.js
+++ src/main/resources/js/action.js
@@ -4,13 +4,23 @@ btn.addEventListener('click',() =>{
     let numbers = document.getElementById('numbers').value;
     axios.post('http://fathomless-bayou-96611.herokuapp.com/calculate', numbers)
         .then(res => {
-            $('.mean').html('');
-            $('.std').html('');
-            let data = JSON.parse(res.data);
-            $(".mean").append( "Arithmetic mean: " + data.mean );
-            $(".std").append( "Standard Deviation :" + data.std);
+            doPost(res);
         })
         .catch( error =>{
             console.log(error);
         })
+    axios.post('http://localhost:5000/calculate', numbers)
+        .then(res => {
+            doPost(res);
+        })
+        .catch( error =>{
+            console.log(error);
+        })
+    function doPost(res) {
+        $('.mean').html('');
+        $('.std').html('');
+        let data = JSON.parse(res.data);
+        $(".mean").append( "Arithmetic mean: " + data.mean );
+        $(".std").append( "Standard Deviation :" + data.std);
+    }
 });
