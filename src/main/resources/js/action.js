61434145cfbcd4852cb1873c0abbbdeee0975f58
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-08-21 00:12:44 -0500

    Logica

--- src/main/resources/js/action.js
+++ src/main/resources/js/action.js
@@ -2,11 +2,13 @@ let btn = document.getElementById('buttonB');
 
 btn.addEventListener('click',() =>{
     let numbers = document.getElementById('numbers').value;
-    axios.post('http://fathomless-bayou-96611.herokuapp.com/calculate', numbers)
+    axios.post('http://localhost:5000/calculate', numbers)
         .then(res => {
+            $('.mean').html('');
+            $('.std').html('');
             let data = JSON.parse(res.data);
-            $(".mean").append( "<span>Arithmetic mean: " + data.mean + "</span>");
-            $(".std").append( "<span>Standard Deviation :" + data.std + "</span>");
+            $(".mean").append( "Arithmetic mean: " + data.mean );
+            $(".std").append( "Standard Deviation :" + data.std);
         })
         .catch( error =>{
             console.log(error);
